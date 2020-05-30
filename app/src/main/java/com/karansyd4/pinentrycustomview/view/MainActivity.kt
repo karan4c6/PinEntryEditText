c9c90f3ea71d058f8135e6a342ed3b830079887b
Karan Sharma <karan.sharma@arq.group>
2020-05-30 16:20:39 +1000

    Reformat Changes

--- app/src/main/java/com/karansyd4/pinentrycustomview/view/MainActivity.kt
+++ app/src/main/java/com/karansyd4/pinentrycustomview/view/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.karansyd4.pinentrycustomview.R
-import kotlinx.android.synthetic.main.activity_pin_entry.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
